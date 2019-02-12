2528983bd6e54c4e21753d942cd0504292e1daef
House <HOUSECH@KCO.AD>
2019-02-12 11:15:48 -0600

    User menu functionality and styling

--- src/components/AuthYes.js
+++ src/components/AuthYes.js
@@ -3,15 +3,37 @@ import UserAvatar from "./UserAvatar";
 import UserAlerts from "./UserAlerts";
 import UserMenu from "./UserMenu";
 
-const AuthYes = () => {
-    return (
-        <div className="auth auth-yes">
-            <i className="fas fa-chevron-down" />
-            <UserAvatar />
-            <UserAlerts />
-            <UserMenu />
-        </div>
-    );
-};
+class AuthYes extends React.Component {
+    state = {
+        menuIsActive: false
+    };
+
+    menuToggle = () => {
+        this.setState({
+            menuIsActive: !this.state.menuIsActive
+        });
+    };
+
+    render() {
+        return (
+            <div className="auth auth-yes">
+                <i
+                    className={
+                        "fas fa-chevron-down" +
+                        (this.state.menuIsActive ? " user-menu-open" : "")
+                    }
+                    onClick={this.menuToggle}
+                />
+                <UserAvatar />
+                <UserAlerts />
+                {this.state.menuIsActive ? (
+                    <UserMenu menuToggle={this.menuToggle} />
+                ) : (
+                    ""
+                )}
+            </div>
+        );
+    }
+}
 
 export default AuthYes;
--- src/components/UserMenu.js
+++ src/components/UserMenu.js
@@ -1,19 +1,56 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class UserMenu extends React.Component {
-    state = {
-        isActive: true
+    // Define the event listener for when the body is clicked (to close the user menu)
+    bodyClick = e => {
+        if (
+            !e.target.classList.contains("user-menu") &&
+            !e.target.classList.contains("user-menu-link") &&
+            !e.target.classList.contains("user-menu-open")
+        ) {
+            this.props.menuToggle();
+        }
     };
+    // Add the click event listener to body when the user menu mounts
+    componentDidMount() {
+        let body = document.querySelector("body");
+        body.addEventListener("click", this.bodyClick);
+    }
+    // And then remove it when the user menu will unmount
+    componentWillUnmount() {
+        let body = document.querySelector("body");
+        body.removeEventListener("click", this.bodyClick);
+    }
 
     render() {
         return (
-            <div
-                className={"user-menu" + (this.state.isActive ? " active" : "")}
-            >
-                ... user menu goes here...
-            </div>
+            <nav className="user-menu">
+                <ul>
+                    <li>
+                        <a href="#" className="user-menu-link">
+                            <i className="far fa-user-circle" /> Profile
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="user-menu-link">
+                            <i className="fas fa-cog" /> Account
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="user-menu-link">
+                            <i className="fas fa-sign-out-alt" /> Log out
+                        </a>
+                    </li>
+                </ul>
+            </nav>
         );
     }
 }
+
+UserMenu.propTypes = {
+    active: PropTypes.bool,
+    menuToggle: PropTypes.func
+};
 
 export default UserMenu;
